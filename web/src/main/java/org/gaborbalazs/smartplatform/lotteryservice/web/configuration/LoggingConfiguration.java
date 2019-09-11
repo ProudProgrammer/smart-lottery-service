8da4ce9f749dad1ece409ce065c25aef3161cedc
Balázs Gábor <mail.gabor.balazs@gmail.com>
2019-09-11 20:45:20 +0200

    Extract logging filter to separate artifact

--- web/src/main/java/org/gaborbalazs/smartplatform/lotteryservice/web/configuration/LoggingConfiguration.java
+++ web/src/main/java/org/gaborbalazs/smartplatform/lotteryservice/web/configuration/LoggingConfiguration.java
@@ -1,7 +1,7 @@
 package org.gaborbalazs.smartplatform.lotteryservice.web.configuration;
 
-import org.gaborbalazs.smartplatform.lotteryservice.web.factory.LogTextFactory;
-import org.gaborbalazs.smartplatform.lotteryservice.web.filter.CustomLoggingFilter;
+import org.gaborbalazs.smartplatform.loggingfilter.factory.LogTextFactory;
+import org.gaborbalazs.smartplatform.loggingfilter.filter.CustomLoggingFilter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
