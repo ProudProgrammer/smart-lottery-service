347694d3154aa98d6fb4da8dfe1985c4fc63d5b9
Gábor Balázs <gbazsi82@gmail.com>
2019-11-14 18:34:13 +0100

    Spring Boot Actuator

--- application/src/main/java/org/gaborbalazs/smartplatform/lotteryservice/application/configuration/SwaggerConfiguration.java
+++ application/src/main/java/org/gaborbalazs/smartplatform/lotteryservice/application/configuration/SwaggerConfiguration.java
@@ -18,7 +18,7 @@ import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-@Profile("!prod")
+@Profile("!prod & !integration-test")
 @Configuration
 @EnableSwagger2
 class SwaggerConfiguration {
@@ -56,7 +56,7 @@ class SwaggerConfiguration {
     Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.withClassAnnotation(RestController.class))
+                .apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.any())
                 .build()
                 .apiInfo(apiInfo());
