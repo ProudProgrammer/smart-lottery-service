997702ab83de9302d0aa370be2fd1c6e184d1b61
Balazs Gabor <v-bgabor@hotels.com>
2020-01-08 11:52:27 +0100

    Given-When-Then formula for unit tests

--- service/src/test/java/org/gaborbalazs/smartplatform/lotteryservice/service/generator/impl/DefaultLotteryNumberGeneratorStrategyTest.java
+++ service/src/test/java/org/gaborbalazs/smartplatform/lotteryservice/service/generator/impl/DefaultLotteryNumberGeneratorStrategyTest.java
@@ -24,6 +24,9 @@ class DefaultLotteryNumberGeneratorStrategyTest {
 
     @Test
     void testGenerateShouldThrowExceptionWhenQuantityLargerThenPoolSize() {
+        // GIVEN
+        // WHEN
+        // THEN
         Assertions.assertThrows(IllegalArgumentException.class, () -> {
             underTest.generate(10, 9);
         });
@@ -31,21 +34,33 @@ class DefaultLotteryNumberGeneratorStrategyTest {
 
     @Test
     void testGenerateShouldReturn5ElementWhenQuantity5() {
+        // GIVEN
+        // WHEN
         var result = underTest.generate(5, 90);
+
+        // THEN
         Assertions.assertEquals(result.size(), 5);
         result.forEach(number -> Assertions.assertTrue(number > 0 && number <= 90));
     }
 
     @Test
     void testGenerateShouldReturn6ElementWhenQuantity6() {
+        // GIVEN
+        // WHEN
         var result = underTest.generate(6, 45);
+
+        // THEN
         Assertions.assertEquals(result.size(), 6);
         result.forEach(number -> Assertions.assertTrue(number > 0 && number <= 45));
     }
 
     @Test
     void testGenerateShouldReturn7ElementWhenQuantity7() {
+        // GIVEN
+        // WHEN
         var result = underTest.generate(7, 35);
+
+        // THEN
         Assertions.assertEquals(result.size(), 7);
         result.forEach(number -> Assertions.assertTrue(number > 0 && number <= 35));
     }
